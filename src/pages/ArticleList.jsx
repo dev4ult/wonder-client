0266825a3dd6fbf2a809757ad4fb6d2d65f92eea
dev4ult <nibrasbiasaaja@gmail.com>
2023-06-27 05:47:44 +0700

    react router dom

---
+++ src/pages/ArticleList.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Article_List() {
+  return <div>Article_List</div>;
+}
+
+export default Article_List;
---
+++ src/pages/NotFound404.jsx
@@ -0,0 +1,5 @@
+function NotFound404() {
+  return <div>NotFound404</div>;
+}
+
+export default NotFound404;
