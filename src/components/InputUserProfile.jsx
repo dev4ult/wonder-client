89a6e34b5cd5faa40421d3a16308a689a86629d6
dev4ult <nibrasbiasaaja@gmail.com>
2023-07-03 22:03:44 +0700

    profile

---
+++ src/components/InputUserProfile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function InputUserProfile({ label, name, value, isTextArea = false }) {
+  return (
+    <div className="form-control">
+      <label className="label-text text-black/30" htmlFor={name}>
+        {label}
+      </label>
+      {isTextArea ? (
+        <textarea name={name} id={name} className="textarea border-gray-300 text-base resize-none h-36">
+          {value}
+        </textarea>
+      ) : (
+        <input type="text" name={name} id={name} className="input border-gray-300" value={value} />
+      )}
+    </div>
+  );
+}
+
+export default InputUserProfile;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,25 @@
 import Navbar from '../components/Navbar';
+import InputUserProfile from '../components/InputUserProfile';
+import ProfileHistory from '../components/ProfileHistory';
+
+import { AiFillHeart } from 'react-icons/ai';
 
 function Profile() {
   return (
     <div>
       <Navbar />
-      Profile
+      <div className="mt-7 flex gap-8">
+        <div className="flex pr-8 flex-col gap-5 w-80 border-r-2">
+          <h2 className="font-semibold text-lg">Edit Profil</h2>
+          <InputUserProfile label="Username" name="username" value="NibrasAlyassar" />
+          <InputUserProfile label="Email" name="email" value="nibras@example.com" />
+          <InputUserProfile label="Bio" name="bio" value="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, iste!" isTextArea={true} />
+          <button className="btn btn-primary btn-outline px-5 btn-sm capitalize rounded-full w-fit">simpan</button>
+        </div>
+        <div className="flex flex-col gap-3">
+          <ProfileHistory title="Disukai" description="Lorem ipsum dolor sit amet consectetur" icon={<AiFillHeart />} amount="10" />
+        </div>
+      </div>
     </div>
   );
 }
