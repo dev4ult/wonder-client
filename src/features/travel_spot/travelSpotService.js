d76cd893a5c0b9e06c997024793b63e2749a68c3
dev4ult <nibrasbiasaaja@gmail.com>
2023-07-05 12:40:38 +0700

    travel spot slice fixed

--- src/features/travel_spot/travelSpotService.js
+++ src/features/travel_spot/travelSpotService.js
@@ -3,11 +3,17 @@ import axios from 'axios';
 const endpoint = `${import.meta.env.VITE_BASEURL}/v1`;
 
 const getAllTravelSpots = async () => {
-  const response = await axios.get(`${endpoint}`);
+  const response = await axios.get(`${endpoint}/discover`);
 
   return response.data;
 };
 
-const authService = { getAllTravelSpots };
+const getDetailTravelSpots = async (travelSpotId) => {
+  const response = await axios.get(`${endpoint}/discover/${travelSpotId}`);
+
+  return response.data;
+};
+
+const authService = { getAllTravelSpots, getDetailTravelSpots };
 
 export default authService;
--- src/features/travel_spot/travelSpotSlice.js
+++ src/features/travel_spot/travelSpotSlice.js
@@ -1,14 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import travelSpotService from './travelSpotService';
 
-const getAllTravelSpots = createAsyncThunk('/', async (_, thunkApi) => {
-  try {
-    return await travelSpotService.getAllTravelSpots();
-  } catch (err) {
-    return thunkApi.rejectWithValue(err.message);
-  }
-});
-
 const initialState = {
   travelSpots: null,
   isLoading: false,
@@ -16,6 +8,14 @@ const initialState = {
   isError: false,
   message: '',
 };
+
+const getAllTravelSpots = createAsyncThunk('/', async (_, thunkApi) => {
+  try {
+    return await travelSpotService.getAllTravelSpots();
+  } catch (err) {
+    return thunkApi.rejectWithValue(err.message);
+  }
+});
 
 const travelSpot = createSlice({
   name: 'travel_spot',
@@ -27,15 +27,15 @@ const travelSpot = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      .addCase(getAllPlaces.isLoading, (state) => {
+      .addCase(getAllTravelSpots.pending, (state) => {
         state.isLoading = true;
       })
-      .addCase(getAllPlaces.isSuccessfull, (state, action) => {
+      .addCase(getAllTravelSpots.fulfilled, (state, action) => {
         state.isLoading = false;
         state.travelSpots = action.payload;
         state.isSuccessfull = true;
       })
-      .addCase(getAllPlaces.isError, (state, action) => {
+      .addCase(getAllTravelSpots.rejected, (state, action) => {
         state.isLoading = false;
         state.travelSpots = null;
         state.isError = true;
